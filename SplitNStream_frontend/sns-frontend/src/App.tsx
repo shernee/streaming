06f235deeb597b5ed0f78362a7f551cde03b2f57
Neetha Sherra <shernee93@gmail.com>
2022-11-11 20:51:08 -0800

    page for group list

--- SplitNStream_frontend/sns-frontend/src/App.tsx
+++ SplitNStream_frontend/sns-frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Login } from 'pages/login'
 import { Home } from 'pages/home'
+import { GroupList } from 'pages/group-list';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />}/>
         <Route path="/home" element={<Home />}/>
+        <Route path="/group-list" element={<GroupList />} />
       </Routes>
     </div>
   );
