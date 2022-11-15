a94d452991939a603f2251ff878c45ffffe199f6
Neetha Sherra <shernee93@gmail.com>
2022-11-15 15:19:35 -0800

    login frontend

--- SplitNStream_frontend/sns-frontend/src/pages/login/index.tsx
+++ SplitNStream_frontend/sns-frontend/src/pages/login/index.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import { useNavigate } from 'react-router-dom'
 import Cookies from 'js-cookie'
+import 'pages/login/index.css'
 import axios from 'axios'
 axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
 axios.defaults.xsrfCookieName = "csrftoken";
@@ -10,34 +11,28 @@ export const Login = () => {
   const navigate = useNavigate()
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
+  const [errors, setErrors] = useState("")
 
   useEffect(() => {
     const loginUrl = `http://splitnshare.local/api/auth/login/`
     axios.get(loginUrl)
-    console.log(username)
-  }, [username])
+  }, [])
 
-  const handleSubmit = (e: React.SyntheticEvent) => {
+  const handleLogin = (e: React.SyntheticEvent) => {
     e.preventDefault();
     const loginUrl = `http://splitnshare.local/api/auth/login/`
     let formData = new FormData();    
-
     formData.append('username', username);   
     formData.append('password', password);
     formData.append('csrfmiddlewaretoken', Cookies.get('csrftoken') || "");
-    // const loginPostData = {
-    //   username: username,
-    //   password: password,
-    //   csrfmiddlewaretoken: Cookies.get('csrftoken')
-    // }
     console.log('In login')
     const userLogin = async() => {
       axios.post(loginUrl, formData).then((loginResponse) => {
-        if(loginResponse.status===202) {
+        if(loginResponse.status === 202) {
           navigate('/home')
-        } else {
-          console.log(loginResponse.data)
-        }
+        }   
+      }).catch(error => {
+        setErrors("Please enter the right username and password")
       })
       
     }
@@ -45,8 +40,8 @@ export const Login = () => {
   }
 
   return (
-    <div className='login-form'>
-      <form onSubmit={handleSubmit}>
+    <div className="login-page">
+      <div className='cover'>
         <input 
           type='text' 
           id='username' 
@@ -55,14 +50,16 @@ export const Login = () => {
           onChange={(e) => {setUsername(e.target.value)}}
           />
         <input 
-          type='text' 
+          type='password' 
           id='password' 
           name='password' 
           placeholder='password' 
           onChange={(e) => {setPassword(e.target.value)}}
           />
-        <input type='submit' value='Login'/>
-      </form>
+          {!!errors && <p>{errors}</p>}
+          <div className="login-btn" onClick={(e) => handleLogin(e)}>Login</div>
+          <p className="text">Not a member yet? Signup!</p>
+      </div>
     </div>
   )
 }
